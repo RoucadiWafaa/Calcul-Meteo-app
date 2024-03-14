3902946d702af78e855bcf1bf59c2440e0270871
Wafaa Rouçadi <roucadiwafaa@gmail.com>
2024-03-14 23:30:51 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calcul_et_meteo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
