e9286d059f859fb7adae714bd52a568c09e2de17
Wafaa Rouçadi <roucadiwafaa@gmail.com>
2024-03-17 03:16:05 +0000

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  op1: number = 0;
+  op2: number = 0;
+  rslt!: number;
 
-  constructor() {}
+  constructor(private router: Router, public toastController: ToastController) {}
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  calculate(operation: (a: number, b: number) => number) {
+    try {
+      if (this.op1 == null || this.op2 == null) {
+        throw new Error('Please enter both numbers.');
+      }
+      this.rslt = operation(this.op1, this.op2);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.presentToast(error.message);
+      } else {
+        this.presentToast('An unexpected error occurred.');
+      }
+    }
+  }
+
+
+  addition() {
+    this.calculate((a, b) => a + b);
+  }
+
+  multiplication() {
+    this.calculate((a, b) => a * b);
+  }
+
+  soustraction() {
+    this.calculate((a, b) => a - b);
+  }
+
+  division() {
+    if (this.op2 === 0) {
+      this.presentToast('Cannot divide by zero.');
+      return;
+    }
+    this.calculate((a, b) => a / b);
+  }
+
+  clear() {
+    this.op1 = 0;
+    this.op2 = 0;
+    this.rslt = 0;
+  }
+
+  gotowt() {
+    this.router.navigate(['weather']);
+  }
 
 }
---
+++ src/app/weather/weather.page.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonRefresher } from '@ionic/angular';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.page.html',
+  styleUrls: ['./weather.page.scss'],
+})
+export class WeatherPage {
+  ville: string = '';
+  pays: string = '';
+  temp: string = '';
+  pres: string = '';
+  hum: string = '';
+  wind: string = '';
+  temp_min: string = '';
+  temp_max: string = '';
+  weatherData: any = null;
+  backgroundImg: string = '';
+
+  constructor(private httpclient: HttpClient) {}
+
+  getWeatherData(){
+    if (this.ville.trim() !== '') {
+      this.weatherData = null; // Clear the previous data
+      const url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.ville.trim() + "&appid=9a9b477204fec6b73bdb5aa86367bccd&units=metric";
+      this.httpclient.get(url).subscribe(
+        (response) => {
+          this.weatherData = response;
+          this.pays = this.weatherData.sys.country;
+          this.temp = this.weatherData.main.temp;
+          this.pres = this.weatherData.main.pressure;
+          this.hum = this.weatherData.main.humidity;
+          this.wind = this.weatherData.wind.speed;
+          this.temp_min = this.weatherData.main.temp_min;
+          this.temp_max = this.weatherData.main.temp_max;
+          this.updateBackgroundImage(this.weatherData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+
+  doRefresh(event: any) {
+    this.getWeatherData();
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
+
+  updateBackgroundImage(weatherData: any) {
+    const hour = new Date().getHours();
+    const temp = weatherData.main.temp;
+
+    if (hour >= 5 && hour < 17) {
+      this.backgroundImg = temp < 15 ? 'morning_cold.jpg' : 'morning_warm.jpg';
+    } else {
+      this.backgroundImg = temp < 15 ? 'night_cold.jpeg' : 'night_warm.jpeg';
+    }
+  }
+
+  getBackgroundStyle() {
+    return `url('../../assets/backgrounds/${this.backgroundImg}') no-repeat center center / cover`;
+  }
+
+
+}
